a879706bc79ca1c28c9cdb422111ab4417e69e10
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-03-21 12:13:15 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,6 @@ function App() {
 
   return (
     <>
-
-
       <DropDown />
       <Hero />
       <Content />
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 function Content (){
     return(
         <>
-        <div className="px-12 bg-white py-12">
+        <div>
         <h1 className="text-left font-semi-bold  text-xl md:text-5xl mb-4 px-8 py-8">preferred Title</h1>
                 <p className="text-left font-semi-bold text-base md:text-2xl mb-4 px-8 text-gray-500">"change this para -- Photography is more than just capturing images; its about preserving emotions,
                     love, and memories that last a lifetime. Every smile, every glance, and every heartfelt moment tells a unique story,
